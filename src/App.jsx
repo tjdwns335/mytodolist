7c9bfaf7ea4a686fd111bc0c64f68e45230fee54
tjdwns335 <http://github.com/tjdwns335>
2024-01-19 16:04:36 +0900

    왜 초기화 되는가...?

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import 'App.css';
-import { useState } from 'react';
+import "App.css";
+import { useState } from "react";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 1, title: '리엑트 공부를 합시다', detail: '리엑트 공부를 해야합니다.', checked: false },
-    { id: 2, title: '리엑트 공부를 합시다', detail: '리엑트 공부를 해야합니다.', checked: true },
+    { id: 1, title: "리엑트 공부를 합시다", detail: "리엑트 공부를 해야합니다.", checked: false },
+    { id: 2, title: "리엑트 공부를 합시다", detail: "리엑트 공부를 해야합니다.", checked: true },
   ]);
-  const [title, setTitle] = useState('');
-  const [detail, setDetail] = useState('');
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
 
   const changeTitle = (event) => {
     setTitle(event.target.value);
-  }
+  };
   const changeDetail = (event) => {
     setDetail(event.target.value);
-  }
+  };
 
   const addHandler = () => {
     const newTodoList = {
@@ -22,36 +22,35 @@ function App() {
       title,
       detail,
       checked: false,
-    }
+    };
     setTodoList([...todoList, newTodoList]);
-    alert('test');
-  }
+  };
   const removeButton = (id) => {
     const newTodoList = todoList.filter((todoList) => todoList.id !== id)
     setTodoList(newTodoList);
-  }
+  };
   const doneButton = (id) => {
     const updatedTodoList = todoList.map(item =>
       item.id !== id ? { ...item, checked: true } : item
     );
     setTodoList(updatedTodoList);
-  }
+  };
 
   const cancelButton = (id) => {
     const updatedTodoList = todoList.map(item =>
       item.id !== id ? { ...item, checked: false } : item
     );
     setTodoList(updatedTodoList);
-  }
+  };
 
 
   return (
-    <div className='basic'>
-      <header className='header-style'>
+    <div className="basic">
+      <header className="header-style">
         <h1>My Todo List</h1>
         <p>React</p>
       </header>
-      <form className='form-style'>
+      <form className="form-style">
         <div>
           <label className="label-style">제목</label>
           <input
@@ -59,7 +58,7 @@ function App() {
             type="text"
             name="title"
             value={title}
-            className='input-style'
+            className="input-style"
           />
           <label className="label-style">내용</label>
           <input
@@ -67,18 +66,18 @@ function App() {
             type="text"
             name="detail"
             value={detail}
-            className='input-style'
+            className="input-style"
           />
         </div>
         <div >
-          <button onClick={addHandler} className='formButton-style'>추가하기</button>
+          <button onClick={addHandler} className="formButton-style">추가하기</button>
         </div>
       </form>
 
-      <div className='list-content'>
-        {/* TOdo-list */}
+      <div className="list-content">
+        {/* Todo-list */}
         <h2>Todo-List</h2>
-        <div className='todoList-wrap'>
+        <div className="todoList-wrap">
           {
             todoList.filter((item) => {
               return item.checked === false
@@ -97,7 +96,7 @@ function App() {
 
         {/* done-list */}
         <h2>Done-List</h2>
-        <div className='todoList-wrap'>
+        <div className="todoList-wrap">
           {
             todoList.filter((item) => {
               return item.checked === true
@@ -112,7 +111,6 @@ function App() {
               )
             })
           }
-
         </div>
       </div>
     </div>
