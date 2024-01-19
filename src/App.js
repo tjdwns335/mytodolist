522cc22ddc7ddd6c08f61235492857e9b3fa1e9f
tjdwns335 <http://github.com/tjdwns335>
2024-01-19 11:32:03 +0900

    testing

--- src/App.js
+++ src/App.js
@@ -4,69 +4,58 @@ import 'reset.css';
 
 function App() {
   const [todoList, setTodoList] = useState([
-    {
-      id: 1, title: '리엑트를 공부하자',
-      detail: "리엑트 공부를 해야합니다."
-    },
+    { id: 1, title: '리엑트 공부를 합시다', detail: '리엑트 공부를 해야합니다.' }
   ]);
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
 
-  const clickAddButtonHandler = () => {
-    const newTodoList = {
-      id: todoList.length + 1,
-      title,
-      detail,
-    }
-    setTodoList([...todoList, newTodoList]);
-  };
-  const titleChangeHandler = (event) => {
+  const changeTitle = (event) => {
     setTitle(event.target.value);
   }
-  const detailChangeHandler = (event) => {
+  const changeDetail = (event) => {
     setDetail(event.target.value);
   }
 
+  const addHandler = () => {
+    const newTodoList = {
+      id: todoList.length + 1,
+      title,
+      detail
+    }
+    setTodoList([...todoList, newTodoList]);
+  }
 
   return (
-    <div>
-      <div className='basic'>
-        <header className='header-style'>
-          <h1>My Todo List</h1>
-          <p>React</p>
-        </header>
-        <form>
-          <div>
-            <label>제목</label>
-            <input
-              value={title}
-              onChange={titleChangeHandler}
-            />
-            <label>내용</label>
-            <input
-              value={detail}
-              onChange={detailChangeHandler}
-            />
-          </div>
-          <button onClick={clickAddButtonHandler}>추가하기</button>
-        </form>
+    <>
+      <div>
+        <label>제목</label>
+        <input value={title}
+          onChange={changeTitle}
+        />
+        <label>내용</label>
+        <input value={detail}
+          onChange={changeDetail}
+        />
         <div>
-          {
-            todoList.map((item) => {
-              return (
-                <>
-                  <p>{item.id}</p>
-                  <h2>{item.title}</h2>
-                  <p>{item.detail}</p>
-                </>
-              )
-            })
-          }
-
+          <button onClick={addHandler}>추가하기</button>
         </div>
       </div>
-    </div>
-  );
+      <div>
+        {
+          todoList.map((item) => {
+            return (
+              <>
+                <p>{item.id}</p>
+                <h2>{item.title}</h2>
+                <p>{item.detail}</p>
+              </>
+            )
+          })
+        }
+
+      </div>
+    </>
+  )
 }
 
 export default App;
